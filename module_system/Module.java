ed1888df4a307f9e234f16b3c0f0da555fc18a9d
futbolpal <futbolpal@gmail.com>
2012-07-28 16:30:09 -0400

    Commented Module.java
    Signed-off-by: futbolpal <futbolpal@gmail.com>

--- module_system/Module.java
+++ module_system/Module.java
@@ -5,19 +5,44 @@ import javax.swing.ImageIcon;
 import basic_gui.BasicDialog;
 import framework.FrameworkListener;
 
-public interface Module extends FrameworkListener
-{
-  public String getName();
+public interface Module extends FrameworkListener {
 
-  public BasicDialog getOptionsDialog();
+	/**
+	 * Returns the name of the module
+	 * @return
+	 */
+	public String getName();
 
-  public boolean checkForUpdate();
+	/**
+	 * Return a GUI component representing options for this module
+	 * 
+	 * @return
+	 */
+	public BasicDialog getOptionsDialog();
 
-  public void update();
+	/**
+	 * Check to see if updates exist for this module
+	 * 
+	 * @return true if an update exists
+	 */
+	public boolean checkForUpdate();
 
-  public ImageIcon getIcon();
-  
-  public boolean close();
-  
-  //public void save();
+	public void update();
+
+	/**
+	 * An icon representing the module
+	 * 
+	 * @return
+	 */
+	public ImageIcon getIcon();
+
+	/**
+	 * Perform closing operations
+	 * 
+	 * @return true if the module closed properly. All modules must close
+	 *         properly for the framework to close.
+	 */
+	public boolean close();
+
+	// public void save();
 }
